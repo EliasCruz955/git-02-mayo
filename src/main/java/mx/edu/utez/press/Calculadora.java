71562b39e02700d2b1f1a3cc116f02f7f6306be4
Elias Cruz <20253ds049@utez.edu.mx>
2026-05-20 13:50:06 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/mx/edu/utez/press/Calculadora.java
@@ -0,0 +1,12 @@
+package mx.edu.utez.press;
+
+public class Calculadora {
+
+    private double num1;
+    private double num2;
+
+    public Calculadora(double num1, double num2){
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+}
